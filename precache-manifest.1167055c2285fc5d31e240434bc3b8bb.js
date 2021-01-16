4354372b5114b424be8ae85204cb682d6ffbfe4c
miri <56790941+miri-san-so@users.noreply.github.com>
2021-01-16 21:35:45 +0530

    Add files via upload

---
+++ precache-manifest.1167055c2285fc5d31e240434bc3b8bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba82d166054834b6870a",
+    "url": "/css/app.6956b5c7.css"
+  },
+  {
+    "revision": "b2b6fda8014a0de8956264090217c5e2",
+    "url": "/img/bg.b2b6fda8.png"
+  },
+  {
+    "revision": "80a14e2155c824dba1f87b0e7e6cd7e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba82d166054834b6870a",
+    "url": "/js/app.1b4d5c7c.js"
+  },
+  {
+    "revision": "3d964d009b18a1469e2a",
+    "url": "/js/chunk-vendors.ceedd823.js"
+  },
+  {
+    "revision": "30878a20238e0f67d652ea2919e02f19",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
